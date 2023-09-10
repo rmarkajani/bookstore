d77c3c118d5f366a6c7cc80c98e43dff8022a173
Jose Estrella <estrellajosem@gmail.com>
2023-09-10 16:58:41 -0600

    updated navbar

--- src/app/main/components/navbar/navbar.component.ts
+++ src/app/main/components/navbar/navbar.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
+  routes = [
+    {
+      title: 'Home',
+      path: 'home',
+    },
+    {
+      title: 'Categories',
+      path: 'categories',
+    },
+    {
+      title: 'About Us',
+      path: 'about-us',
+    },
+    {
+      title: 'Contact Us',
+      path: 'contact-us',
+    },
+  ];
 }
