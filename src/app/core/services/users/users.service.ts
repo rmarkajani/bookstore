f09bd872fae49515664f133c0a308bb5451bc3e3
Jose Estrella <estrellajosem@gmail.com>
2023-09-22 20:14:11 -0600

    checkpoint

---
+++ src/app/core/services/users/users.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  http = inject(HttpClient);
+  constructor() {}
+
+  URL = 'http://localhost:3000/users';
+  getUsers() {
+    return this.http.get(this.URL);
+  }
+}
--- src/app/main/pages/profile/profile.component.ts
+++ src/app/main/pages/profile/profile.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UsersService } from 'src/app/core/services/users/users.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent {
-
+  userService = inject(UsersService);
+  ngOnInit() {
+    const users = this.userService.getUsers();
+    console.log(users);
+  }
 }
